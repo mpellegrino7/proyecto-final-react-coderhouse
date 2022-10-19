c4d6fae8dbbc4ad2aaa1b7235397657e7437031b
Matias Pellegrino <matiaspellegrino@outlook.com>
2022-10-18 22:26:16 -0300

    Componentes II

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const ItemCount = ({ initial, stock, onAdd }) => {
+  const [count, setCount] = useState(initial)
+
+  const increase = () => {
+    if (count < stock) {
+      setCount(count + 1)
+    }
+  }
+
+  const decrease = () => {
+    if (count > initial) {
+      setCount(count - 1)
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <p>Remera ejemplo</p>
+      </div>
+      <button onClick={decrease}>-</button>
+      <div>{count}</div>
+      <button onClick={increase}>+</button>
+      <div>
+        <button onClick={() => onAdd(count)}>Agregar al Carrito</button>
+      </div>
+    </>
+  )
+}
+
+export default ItemCount
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
+
+const onAdd = (count) => {
+  console.log(`${count} productos fueron agregados al carrito`)
+}
 
 const ItemListContainer = ({ greeting }) => {
-  return <h1>{greeting}</h1>
+  return (
+    <>
+      <h1>{greeting}</h1>
+      <ItemCount initial={1} stock={15} onAdd={onAdd} />
+    </>
+  )
 }
 
 export default ItemListContainer
